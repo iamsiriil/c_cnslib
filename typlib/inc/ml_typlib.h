d17600130e10edce299f093d22e8393ecde0b3a8
siriil <siriil@protonmail.ch>
2025-01-29 18:21:47 +0000

    update: typlib header file

---
+++ typlib/inc/ml_typlib.h
@@ -0,0 +1,12 @@
+#ifndef ML_TYPLIB_H
+# define ML_TYPLIB_H
+
+int	ml_isalnum(int c); 
+int	ml_isalpha(int c);
+int	ml_isascii(int c);
+int	ml_isdigit(int c);
+int	ml_isprint(int c);
+int	ml_tolower(int c);
+int	ml_toupper(int c);
+
+#endif
